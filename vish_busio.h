2ff286f085beafbb37105c18aa30b1b40405bf82
Vishwam Aggarwal <58611588+vishwam-aggarwal@users.noreply.github.com>
2024-02-22 01:06:32 -0800

    Add files via upload
    Initial Commit

---
+++ vish_busio.h
@@ -0,0 +1,14 @@
+#ifndef _VISH_BUSIO_H
+#define _VISH_BUSIO_H
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "vish_i2cdev.h"
+
+class vish_busio
+{
+    public:
+    vish_busio();
+};
+
+#endif // _VISH_BUSIO_H
