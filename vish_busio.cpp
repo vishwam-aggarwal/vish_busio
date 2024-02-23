d2995688e286365fac4373b633ef7c6b51cce94b
Vishwam Aggarwal <vishwam.aggarwal@gmail.com>
2024-02-23 05:28:36 -0800

    Removing example constructor code

--- vish_busio.cpp
+++ vish_busio.cpp
@@ -6,16 +6,4 @@
 
 vish_busio::vish_busio()
 {
-    stdio_init_all();
-    vish_i2cdev myI2C;
-
-    myI2C.begin(0x40, I2C_PORT, 400*1000, I2C_SDA, I2C_SCL, false);
-
-    uint8_t Data[2];
-    uint8_t reg = 0x00;    
-
-    // Read Config register
-    myI2C.read(&reg, &Data[0], 2);
-    uint16_t configReg = (int16_t)((Data[0] << 8) | Data[1]);
-
 }
