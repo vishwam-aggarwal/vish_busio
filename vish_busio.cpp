55c243b237597092be143b64075689f60bbd2474
Vishwam Aggarwal <vishwam.aggarwal@gmail.com>
2024-02-23 17:44:39 -0800

    Buggy implementation for Bus Register.

--- vish_busio.cpp
+++ vish_busio.cpp
@@ -1,9 +1,44 @@
 #include "vish_busio.h"
 
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+vish_BusIO_Register::vish_BusIO_Register(vish_i2cdev* INA260_I2C, uint32_t reg, uint8_t width,
+                                        bool byteorder, uint8_t reg_len, bool reg_byteorder) {
+    _INA260_I2C = INA260_I2C;
+    _reg_len = reg_len;
+    _reg_byteorder = reg_byteorder;
+    _byteorder = byteorder;
+    _width = width; // BUG! BUG! BUG! BUG! BUG! BUG! BUG!
 
-vish_busio::vish_busio()
+    if (_reg_byteorder) {
+        _reg[0] = reg >> 24;
+        _reg[1] = (reg >> 16) & 0xFF;
+        _reg[2] = (reg >> 8) & 0xFF;
+        _reg[3] = reg & 0xFF;
+    }
+    else
+    {
+        _reg[4] = reg >> 24;
+        _reg[3] = (reg >> 16) & 0xFF;
+        _reg[2] = (reg >> 8) & 0xFF;
+        _reg[1] = reg & 0xFF;
+    }
+
+}
+
+uint32_t vish_BusIO_Register::read(void)
 {
+    uint8_t dbg = 0x00;
+    uint8_t dbg1[2];
+    uint8_t _dat[_width];
+    uint32_t readData;
+    //_INA260_I2C->read(&_reg[0], &_dat[0], _width, _reg_len);
+    if(_byteorder)
+        readData = (int32_t) ((_dat[0]<<24) |   (_dat[1]<<16)   |   (_dat[2] << 8)  |   _dat[3]);
+    else
+        readData = (int32_t) ((_dat[3]<<24) |   (_dat[2]<<16)   |   (_dat[1] << 8)  |   _dat[0]);
+
+    _INA260_I2C->read(&dbg, &dbg1[0], _width, _reg_len);
+    readData = int16_t((dbg1[0]<<8) | (dbg1[1]));
+
+    return readData;
+
 }
--- vish_busio.h
+++ vish_busio.h
@@ -1,13 +1,52 @@
 #ifndef _VISH_BUSIO_H
 #define _VISH_BUSIO_H
 
-#include "hardware/i2c.h"
 #include "vish_i2cdev.h"
 
-class vish_busio
-{
-    public:
-    vish_busio();
+enum ByteOrder {
+  LSBFIRST = false, ///< Active high open-collector (Default)
+  MSBFIRST = true, ///< Active low open-collector
+};
+
+/*!
+ * @brief The class which defines a device register (a location to read/write
+ * data from)
+ */
+class vish_BusIO_Register {
+public:
+  vish_BusIO_Register(vish_i2cdev* INA260_I2C, uint32_t reg, uint8_t width = 2, bool byteorder = LSBFIRST,
+                          uint8_t reg_len = 1, bool reg_byteorder = LSBFIRST);
+
+  uint32_t read(void);
+  bool write(uint32_t value, uint8_t numbytes = 0);
+
+  //uint8_t width(void);
+
+  void setWidth(uint8_t width);
+  void setAddress(uint16_t address);
+  void setAddressWidth(uint16_t address_width);
+
+private:
+    vish_i2cdev* _INA260_I2C;
+    uint8_t _reg[4];
+    uint8_t _width, _reg_len;
+    bool _byteorder, _reg_byteorder;
+};
+
+/*!
+ * @brief The class which defines a slice of bits from within a device register
+ * (a location to read/write data from)
+ */
+class vish_BusIO_RegisterBits {
+public:
+  vish_BusIO_RegisterBits(vish_BusIO_Register *reg, uint8_t bits,
+                              uint8_t shift);
+  bool write(uint32_t value);
+  uint32_t read(void);
+
+private:
+  vish_BusIO_Register* _register;
+  uint8_t _bits, _shift;
 };
 
 #endif // _VISH_BUSIO_H
